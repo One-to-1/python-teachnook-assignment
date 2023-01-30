d12039ff999e6e2f10754edb1636c4efd08fcbd8
Oneto1 <99424627+One-to-1@users.noreply.github.com>
2023-01-30 15:09:37 +0530

    1st commit

---
+++ assignment-1.py
@@ -0,0 +1,7 @@
+# Assignment 1 
+# Create a two different examples for collective datatypes in python
+
+
+Uni={'Jawaharlal Nehru University','Delhi University','Amity University','Kanpur University','Kalinga University'}
+
+Courses=['B.Tech','M.Tech','MBA','MCA','BBA']
